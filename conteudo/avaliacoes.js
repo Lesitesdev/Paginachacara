013db80b107b11d14c4b8a4f3a12e5528595c8fe
Leandro Bueno <lesites.dev@gmail.com>
2026-06-19 19:40:23 -0300

    Add verified Airbnb guest reviews

--- conteudo/avaliacoes.js
+++ conteudo/avaliacoes.js
@@ -6,4 +6,29 @@
  * { nome: "Nome do cliente", evento: "Tipo de evento", nota: 5,
  *   comentario: "Comentário do cliente." }
  */
-window.AVALIACOES = [];
+window.AVALIACOES = [
+  {
+    nome: "Rafa",
+    evento: "Airbnb",
+    nota: 5,
+    comentario: "Fomos pegos de surpresa pela ladeira, dentro da propriedade, que tivemos que subir com o carro para chegar ao local da festa, mas uma vez lá, tivemos uma ótima experiência. Meus convidados amaram, pois puderam descansar em paz e com bastante espaço. Os espaços para os churrascos estavam ótimos."
+  },
+  {
+    nome: "Vinicius",
+    evento: "Airbnb · agosto de 2025 · Ficou uma noite",
+    nota: 5,
+    comentario: "Sidnei e Rafael foram muito solícitos e nos apoiaram desde o fechamento da reserva até a finalização da locação. O espaço é bem reservado e tranquilo. O Rafael explicou com muita clareza todas as informações. Excelente, e voltarei com certeza!"
+  },
+  {
+    nome: "Rafael",
+    evento: "Airbnb · abril de 2024 · Ficou uma noite",
+    nota: 5,
+    comentario: "A estadia foi ótima, o espaço condiz com o anunciado e supriu todas as expectativas. O caseiro e o Sidnei foram muito solícitos e acolhedores, sempre disponíveis para tirar qualquer dúvida."
+  },
+  {
+    nome: "Renato",
+    evento: "Airbnb · dezembro de 2023 · Ficou uma noite",
+    nota: 5,
+    comentario: "Sidnei foi um ótimo anfitrião, apoiando em todo o processo, desde o início até o fim da hospedagem."
+  }
+];
