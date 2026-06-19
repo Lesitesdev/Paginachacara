59a92d9d75dca21966a38908c19b15d960aaa6c7
Leandro Bueno <lesites.dev@gmail.com>
2026-06-19 16:09:53 -0300

    Add gallery navigation and editable reviews

---
+++ conteudo/avaliacoes.js
@@ -0,0 +1,9 @@
+/*
+ * COMENTÁRIOS E AVALIAÇÕES
+ * Adicione somente avaliações reais e autorizadas pelos clientes.
+ * Copie o modelo abaixo para dentro da lista e remova o // de cada linha:
+ *
+ * { nome: "Nome do cliente", evento: "Tipo de evento", nota: 5,
+ *   comentario: "Comentário do cliente." }
+ */
+window.AVALIACOES = [];
---
+++ conteudo/galeria.js
@@ -0,0 +1,17 @@
+/*
+ * GALERIA DO SITE
+ * Para adicionar uma foto:
+ * 1. Coloque o arquivo dentro da pasta img.
+ * 2. Copie um item abaixo e altere src e alt.
+ * 3. Mantenha a vírgula entre os itens.
+ */
+window.GALERIA_FOTOS = [
+  { src: "img/Galeria de fotos/Piscina.JPG", alt: "Piscina do Infinity Dreams" },
+  { src: "img/Galeria de fotos/Salao.JPG", alt: "Salão coberto" },
+  { src: "img/Galeria de fotos/Cozinha.JPG", alt: "Cozinha equipada" },
+  { src: "img/Galeria de fotos/Quartos.JPG", alt: "Quarto da chácara" },
+  { src: "img/Galeria de fotos/foto5.JPG", alt: "Área externa da chácara" },
+  { src: "img/Galeria de fotos/Campo.JPG", alt: "Campo gramado" },
+  { src: "img/Galeria de fotos/foto piscina.JPG", alt: "Vista da piscina e da casa" },
+  { src: "img/Galeria de fotos/foto7.JPG", alt: "Espaço ao ar livre" }
+];
